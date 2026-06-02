c64dae1e059f6a6a934e9d13dfcbfbe131fc5cd1
Rey-3 <zhiboon2003@gmail.com>
2026-06-02 20:10:15 +0800

    feat(lecturer): add lecturer layout, sidebar, and topbar chrome

---
+++ src/controls/lecturer_sidebar.ascx.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Web.UI;
+
+namespace src.controls
+{
+    public partial class lecturer_sidebar : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e) { }
+    }
+}
